0b1bb5dedd3f6ce4316545f200af229f6a026bce
Pip <pip.harsveld@hva.nl>
2023-03-20 13:51:14 +0100

    Implement handlebars with partials

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import path from 'path';
 import express from 'express';
+import handlebars from 'express-handlebars';
+
 
 const app = express();
 const port = 5000;
 const __dirname = path.resolve();
 
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/static'));
 
+app.engine('hbs', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: __dirname + '/views/partials'
+    }));
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-});
+    res.render('main', {layout : 'index'});
+    });
+    
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
