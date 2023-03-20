8bd70549dfca55be987b922d5480cf7448cee048
Pip <pip.harsveld@hva.nl>
2023-03-20 13:24:41 +0100

    Add style to project

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express');
+import path from 'path';
+import express from 'express';
+
 const app = express();
 const port = 5000;
+const __dirname = path.resolve();
 
+
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: __dirname});
