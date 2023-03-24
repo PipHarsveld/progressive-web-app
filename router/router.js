bf24633dd5a710986acdd07d4ad938746f1b77b1
Pip <pip.harsveld@hva.nl>
2023-03-24 10:36:44 +0100

    Load dynamic data on details page

--- router/router.js
+++ router/router.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import request from 'request';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = express.Router();
 
@@ -8,7 +11,7 @@ router.get('/', (req, res) => {
 })
 
 router.get('/overview', (req, res) => {
-    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=RYeqgpSb&ps=6`, {json: true}, function (err, response, data){
+    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&ps=6`, {json: true}, function (err, response, data){
         if (err) {
             res.send(err);
             console.error('error:', error);
@@ -20,8 +23,22 @@ router.get('/overview', (req, res) => {
     })
 });
 
-router.get('/details', (req, res) => {
-    res.render('details', { layout: 'index' });
+router.get('/details/:id', (req, res) => {
+    console.log("test params", req.params.id);
+    const idWithPrefix = req.params.id;
+    const id = idWithPrefix.replace('nl-', '');
+
+    request(`https://www.rijksmuseum.nl/api/nl/collection/${id}?key=${process.env.API_KEY}`, {json: true}, function (err, response, data){
+        if (err) {
+            res.send(err);
+            console.error('error:', error);
+        } else {
+            const details = data.artObject;
+            console.log(details.title);
+            res.render('details', { layout: 'index', data: details});
+        }
+    })
+
 });
 
 
@@ -29,7 +46,7 @@ router.get('/categorie/:type', (req, res) => {
     const typeArt = req.params['type'];
     console.log(typeArt);
 
-    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=RYeqgpSb&type=${typeArt}`, {json: true}, function (err, response, data){
+    request(`https://www.rijksmuseum.nl/api/nl/collection/?key=${process.env.API_KEY}&type=${typeArt}`, {json: true}, function (err, response, data){
         if (err) {
             res.send(err);
             console.error('error:', error);
@@ -39,16 +56,6 @@ router.get('/categorie/:type', (req, res) => {
             res.render('paintings', {layout : 'index', data: specificArtPieces});
         }
     })
-});
-
-
-
-router.get('/schilderijen', (req, res) => {
-    
-});
-
-router.get('/sculpturen', (req, res) => {
-    
 });
 
 
